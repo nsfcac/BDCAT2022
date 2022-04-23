4c78517f6a7580f5d93328a7b65c75bcc26cd80d
Zipexpo <nvtngan@hcmus.edu.vn>
2022-04-23 02:06:27 -0500

    Updates

---
+++ precache-manifest.abd3e6e378ebc7476736bb5bf701be32.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6717972a52b09f93af8b20dac29c8ca7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b78f468c81a1608d839e",
+    "url": "./static/css/main.de1fa8c9.chunk.css"
+  },
+  {
+    "revision": "2e97954869f62c1d35f3",
+    "url": "./static/js/2.48904a8f.chunk.js"
+  },
+  {
+    "revision": "b78f468c81a1608d839e",
+    "url": "./static/js/main.1ab56230.chunk.js"
+  },
+  {
+    "revision": "61e1d483a48a1f33ab0e",
+    "url": "./static/js/runtime~main.67138cfb.js"
+  },
+  {
+    "revision": "b0a49b6cb663eca681b4a3edc090feb4",
+    "url": "./static/media/ACM1.b0a49b6c.png"
+  },
+  {
+    "revision": "3d4d7535bb2162eac1783ea4cee7f2cd",
+    "url": "./static/media/ACM2.3d4d7535.png"
+  },
+  {
+    "revision": "55024ea8792f6df4414b19d145333373",
+    "url": "./static/media/ICS_stacked-transparent.55024ea8.gif"
+  },
+  {
+    "revision": "fc6bb204d46698e7991fea5f0d614aa3",
+    "url": "./static/media/IEEE_logo.fc6bb204.jpg"
+  },
+  {
+    "revision": "a7479450b598dae6ebea757d588dbdab",
+    "url": "./static/media/campus-entrance-fall.a7479450.png"
+  },
+  {
+    "revision": "58fb4e7bb68986ae7d93f9dbe73001e8",
+    "url": "./static/media/cloudbuslogo.58fb4e7b.png"
+  },
+  {
+    "revision": "877da40572cad8eb928ac7e970f674c1",
+    "url": "./static/media/cta-illustration.877da405.svg"
+  },
+  {
+    "revision": "c0030329a697fbfbd2337cd0f16cd821",
+    "url": "./static/media/features-split-image.c0030329.svg"
+  },
+  {
+    "revision": "c35dd856ec8eb2b4f3d935b6552d33e9",
+    "url": "./static/media/header.c35dd856.jpg"
+  },
+  {
+    "revision": "e22ec14c8d912e8f793a37181e4eea5e",
+    "url": "./static/media/illustration-element-01-top.e22ec14c.svg"
+  },
+  {
+    "revision": "b5e6fc1a482e36c9b6e6141d9dd5d88a",
+    "url": "./static/media/illustration-element-01.b5e6fc1a.svg"
+  },
+  {
+    "revision": "c34c32b991559f9ffce5024da78094ee",
+    "url": "./static/media/illustration-element-02-top.c34c32b9.svg"
+  },
+  {
+    "revision": "1d92e1f3197b6578204b41b38fcb9546",
+    "url": "./static/media/illustration-element-02.1d92e1f3.svg"
+  },
+  {
+    "revision": "41f424139c61a5b5c0b56d7e7f2b3c7a",
+    "url": "./static/media/illustration-element-03.41f42413.svg"
+  },
+  {
+    "revision": "8d90cdb8c7b4d161c5efc5c52d261b1b",
+    "url": "./static/media/illustration-element-04.8d90cdb8.svg"
+  },
+  {
+    "revision": "cf0a1aa107a0479443066051c688f056",
+    "url": "./static/media/illustration-element-05.cf0a1aa1.svg"
+  },
+  {
+    "revision": "3cdb78d299b43be6a48a46163a6dd1dd",
+    "url": "./static/media/illustration-element-06.3cdb78d2.svg"
+  },
+  {
+    "revision": "36b12cc07e44922f8a1f7dd6115ad383",
+    "url": "./static/media/illustration-element-07.36b12cc0.svg"
+  },
+  {
+    "revision": "667ea8e055e7a1a1bc414e43190dde7d",
+    "url": "./static/media/illustration-element-08.667ea8e0.svg"
+  },
+  {
+    "revision": "38a7600ef5c6afb79f77355d284bef2c",
+    "url": "./static/media/illustration-section-01.38a7600e.svg"
+  },
+  {
+    "revision": "b5aed62027896ccbf1b4e9d143a94071",
+    "url": "./static/media/illustration-section-02.b5aed620.svg"
+  },
+  {
+    "revision": "80fdf350045b4310c3dde6508d5738ac",
+    "url": "./static/media/illustration-section-03.80fdf350.svg"
+  },
+  {
+    "revision": "22b7138b291f7f433189fd93d3f9ae03",
+    "url": "./static/media/illustration-section-04.22b7138b.svg"
+  },
+  {
+    "revision": "849d5654ae268da90ab975a0bafe4600",
+    "url": "./static/media/illustration-section-05.849d5654.svg"
+  },
+  {
+    "revision": "66abc7f824c1097641e9fd1bc7daceb2",
+    "url": "./static/media/illustration-section-06.66abc7f8.svg"
+  },
+  {
+    "revision": "9d1da83d3b4fa9ea3c89719f24ac449c",
+    "url": "./static/media/logo.9d1da83d.svg"
+  },
+  {
+    "revision": "9b59296c6a88c24f65bbfdbf728a92d3",
+    "url": "./static/media/members.9b59296c.csv"
+  },
+  {
+    "revision": "747ca59a2fd2bc67677e784532ebf368",
+    "url": "./static/media/universitys.747ca59a.csv"
+  },
+  {
+    "revision": "22e4e6cd0ce1b7684d8aed4f99722b6b",
+    "url": "./static/media/video-placeholder.22e4e6cd.svg"
+  }
+]);
